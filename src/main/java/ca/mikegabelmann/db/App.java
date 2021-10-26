f553ab8cae960d5df77bb14f20183cedcaadf92b
mgabelmann <mgabelmann@gmail.com>
2021-10-26 14:48:03 -0700

    -added a couple of initial templates to create an entity and dao. -contents are written to STDOUT for now.

--- src/main/java/ca/mikegabelmann/db/App.java
+++ src/main/java/ca/mikegabelmann/db/App.java
@@ -1,5 +1,9 @@
 package ca.mikegabelmann.db;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.Version;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBElement;
 import jakarta.xml.bind.Marshaller;
@@ -9,7 +13,13 @@ import org.apache.logging.log4j.Logger;
 import org.apache.torque.TableType;
 
 import javax.xml.namespace.QName;
+import java.io.OutputStreamWriter;
 import java.io.StringWriter;
+import java.io.Writer;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 
 public class App {
@@ -26,7 +36,7 @@ public class App {
         SQLiteFactory factory = new SQLiteFactory();
         factory.parseFile(CharStreams.fromStream(App.class.getResourceAsStream("/example.sqlite")));
 
-        //print XML tree
+        //JAXB - print XML tree
         JAXBContext context = JAXBContext.newInstance(TableType.class);
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
@@ -37,6 +47,31 @@ public class App {
 
         String xml = sw.toString();
         LOG.debug(xml);
+
+
+        //FreeMarker - process templates
+        Configuration cfg = new Configuration();
+        cfg.setClassForTemplateLoading(App.class, "/templates");
+        cfg.setIncompatibleImprovements(new Version(2, 3, 20));
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setLocale(Locale.US);
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+        Map<String, Object> input = new HashMap<>();
+        input.put("table", factory.getTable());
+        input.put("basePackagePath", "ca.mikegabelmann.persistence");
+        input.put("author", "codegenerator");
+        input.put("version", "1.0.0");
+        input.put("buildDtm", LocalDateTime.now());
+
+        //dump output to console
+        Template dao = cfg.getTemplate("dao.ftl");
+        Writer cw = new OutputStreamWriter(System.out);
+        dao.process(input, cw);
+
+        Template entity = cfg.getTemplate("entity.ftl");
+        Writer cw2 = new OutputStreamWriter(System.out);
+        entity.process(input, cw2);
     }
 
 }
